9ea8077326abd03a5c22e605afa6349172ae7a4b
leoozxnn <vanzzleoo@gmail.com>
2025-09-02 20:51:12 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,111 @@
+require("./Zion")
+const fs = require('fs')
+const { version } = require("./package.json")
+//~~~~~~~~~SETTING BOT~~~~~~~~~~//
+
+// Bebas Ubah
+global.owner = "6287863175963"
+global.nobot = "6289525579281"
+global.namaowner = "Lanzz - Arisu-MultiDevice"
+global.namaBot = "Arisu-MultiDevice"
+global.title = "Arisu-MultiDevice"
+
+// Jangan Di ubah
+global.creator = `${owner}@s.whatsapp.net` 
+global.foother = `© ${namaBot}`
+global.versi = "New"
+global.nama = namaBot 
+global.namach = nama 
+global.namafile = foother 
+global.author = namaowner
+
+// Bebas Ubah
+// True = on || False = Off 
+global.status = true
+global.owneroff = true
+global.autoread = true
+global.autotyping = true
+global.Antilinkgc = true
+global.Antilinkch = true
+global.antispam = true
+global.onlygc = false
+
+// Set Payment
+global.qris = "https://files.catbox.moe/x"
+global.dana = "087863175963"
+global.gopay = "087863175963"
+
+// ===={ Set Link }
+global.ch = 'https://whatsapp.com/channel/x'
+global.idch = 'x@newsletter'
+global.linkgc = 'https://chat.whatsapp.com/x'
+global.yt = 'https://youtube.com/@leoganteng1'
+global.nekorin = "https://api.nekorinn.my.id"
+global.idgc = "x@g.us"
+// set prefix
+global.setprefix = ".", "/", "#"
+
+// User Sosmed
+global.tt = "@leooshikigami_"
+global.yt = "@leoganteng1"
+global.ig = "@leozxnn"
+
+// Setting Api cVPS
+global.doToken = "APIKEY"
+global.linodeToken = "APIKEY"
+
+// Settings Api Panel Pterodactyl
+global.egg = "15" // Egg ID
+global.nestid = "5" // nest ID
+global.loc = "1" // Location ID
+global.domain = "https://"
+global.apikey = "ptla" //ptla
+global.capikey = "ptlc" //ptlc
+
+// [ THEME URL & URL ] ========//
+global.thumbnail = 'https://img1.pixhost.to/images/7174/621571722_zion.jpg'
+
+// Settings reply ~~~~~~~~~//
+global.mess = {
+    owner: "Khusus Owner",
+    prem: "Khusus Premium",
+    group: "Khusus di Group Chat",
+    admin: "Khusus Admin",
+    botadmin: "Bot Harus Jadi Admin",
+    private: "Khusus di Private Chat",
+    done: "Sukses"
+}
+
+global.packname = nama
+global.author = namaBot
+
+//
+global.gamewaktu = 60 // Game waktu
+global.suit = {};
+global.tictactoe = {};
+global.petakbom = {};
+global.kuis = {};
+global.siapakahaku = {};
+global.asahotak = {};
+global.susunkata = {};
+global.caklontong = {};
+global.family100 = {};
+global.tebaklirik = {};
+global.tebaklagu = {};
+global.tebakgambar2 = {};
+global.tebakkimia = {};
+global.tebakkata = {};
+global.tebakkalimat = {};
+global.tebakbendera = {};
+global.tebakanime = {};
+global.kuismath = {};
+
+//~~~~~~~~~~~ DIEMIN ~~~~~~~~~~//
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
